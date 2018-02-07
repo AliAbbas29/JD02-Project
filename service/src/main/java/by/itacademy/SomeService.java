2c029d7f1ff08e4ff7fa4d35a79dab97491b58e7
AliAbbas29 <AliAbbas2905@gmail.com>
2018-02-07 13:22:14 +0300

    IT WORKS!! O_O

--- service/src/main/java/by/itacademy/SomeService.java
+++ service/src/main/java/by/itacademy/SomeService.java
@@ -8,15 +8,15 @@ import org.hibernate.cfg.Configuration;
 import java.util.List;
 
  class SomeService {
-     List<Instructor> writeExample() {
+        List<Instructor> writeExample() {
         SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         org.hibernate.query.Query<Instructor> query = session.createQuery("select i from Instructor  i", Instructor.class);
         List<Instructor> instructors = query.getResultList();
-//        for (Instructor i : instructors) {
-//            System.out.println(i);
-//        }
+        for (Instructor i : instructors) {
+            System.out.println(i);
+        }
         session.getTransaction().commit();
         session.close();
         sessionFactory.close();
