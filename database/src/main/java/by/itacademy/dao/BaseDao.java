be25ce5e6413b8e140d119a79f99bf4604449a10
AliAbbas29 <AliAbbas2905@gmail.com>
2018-02-11 11:03:44 +0300

    H2-Base tests

--- database/src/main/java/by/itacademy/dao/BaseDao.java
+++ database/src/main/java/by/itacademy/dao/BaseDao.java
@@ -4,61 +4,57 @@ package by.itacademy.dao;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
 import java.util.List;
 
 public abstract class BaseDao<T> {
+    private static final SessionFactory SESSION_FACTORY = new Configuration().configure().buildSessionFactory();
 
     public void save(T entity) {
-        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        Session session = sessionFactory.openSession();
+        Session session = SESSION_FACTORY.openSession();
         session.beginTransaction();
         session.save(entity);
         session.getTransaction().commit();
         session.close();
-        sessionFactory.close();
     }
 
     public void delete(T entity) {
-        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        Session session = sessionFactory.openSession();
+        Session session = SESSION_FACTORY.openSession();
         session.beginTransaction();
         session.delete(entity);
         session.getTransaction().commit();
         session.close();
-        sessionFactory.close();
     }
 
     public void update(T entity) {
-        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        Session session = sessionFactory.openSession();
+        Session session = SESSION_FACTORY.openSession();
         session.beginTransaction();
         session.update(entity);
         session.getTransaction().commit();
         session.close();
-        sessionFactory.close();
     }
 
     public T findById(int id) {
-        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        Session session = sessionFactory.openSession();
+        Session session = SESSION_FACTORY.openSession();
         session.beginTransaction();
         T entity = session.find(getEntityClass(), id);
         session.getTransaction().commit();
         session.close();
-        sessionFactory.close();
         return entity;
     }
 
     public List<T> findAll() {
-        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-        Session session = sessionFactory.openSession();
+        Session session = SESSION_FACTORY.openSession();
         session.beginTransaction();
         org.hibernate.query.Query<T> query = session.createQuery("select i from " + getEntityClass().getSimpleName() + " i ", getEntityClass());
         List<T> entity = query.getResultList();
         session.getTransaction().commit();
         session.close();
-        sessionFactory.close();
         return entity;
+    }
+
+    public void sessionClose() {
+        SESSION_FACTORY.close();
     }
 
 
--- database/src/test/java/by/itacademy/MappingTest.java
+++ database/src/test/java/by/itacademy/MappingTest.java
@@ -14,19 +14,19 @@ public class MappingTest extends BaseTest {
 
     @Test
     public void CourseTest() {
-        Student sue = new Student("Sue",80253932149L,new Address("London","Trafalguar sq. 14",22));
+        Student sue = new Student("Sue", 80253932149L, new Address("London", "Trafalguar sq. 14", 22));
         StudentDao studentDao = new StudentDao();
         studentDao.save(sue);
-        Assert.assertThat(studentDao.findById(1).getName(),is("Sue"));
+        Assert.assertThat(studentDao.findById(1).getName(), is("Sue"));
         Student sam = studentDao.findById(1);
         sam.setName("Sam");
         studentDao.update(sam);
-        Assert.assertThat(studentDao.findById(1).getName(),is("Sam"));
+        Assert.assertThat(studentDao.findById(1).getName(), is("Sam"));
         studentDao.delete(studentDao.findById(1));
+        studentDao.save(sue);
+        studentDao.save(sam);
         List<Student> students = studentDao.findAll();
-        studentDao.save(sue);
-        studentDao.save(sue);
-        Assert.assertThat(students.get(2).getName(),is("Sue"));
+        Assert.assertThat(students.get(1).getName(), is("Sam"));
+        studentDao.sessionClose();
     }
-
 }
